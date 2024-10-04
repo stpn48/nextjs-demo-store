9b0717f8ab136680c898776eec148425f78633e3
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-04 18:17:36 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+"use client";
+
+import ToggleThemeButton from "@/components/ToggleThemeButton";
+
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <ToggleThemeButton />
+    </>
+  );
 }
