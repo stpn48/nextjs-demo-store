b857e661f66c875ce297c9e37e00471d7cefbd58
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-05 16:15:06 +0200

    Create Carousel.tsx

---
+++ src/app/components/Carousel.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { Product } from "@/types/types";
+import { ProductCard } from "./ProductCard";
+import { useEffect, useRef } from "react";
+
+type Props = {
+  products: Product[];
+};
+
+export function Carousel({ products }: Props) {
+  const carouselRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const carousel = carouselRef.current;
+
+    if (!carousel) return;
+
+    const scrollSpeed = 1.5; // Adjust this value to change the scroll speed
+    let startPosition = 0;
+
+    const scroll = () => {
+      if (carousel.scrollLeft >= carousel.scrollWidth - carousel.offsetWidth) {
+        carousel.scrollLeft = 0; // Reset scroll to the beginning
+      } else {
+        carousel.scrollLeft += scrollSpeed; // Auto-scroll left
+      }
+    };
+
+    const scrollInterval = setInterval(scroll, 16); // 60fps (16ms/frame)
+
+    // Cleanup interval on unmount
+    return () => {
+      clearInterval(scrollInterval);
+    };
+  }, [carouselRef]);
+
+  return (
+    <div className="w-screen overflow-x-scroll" ref={carouselRef}>
+      <div className="flex w-max gap-4">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
+}
