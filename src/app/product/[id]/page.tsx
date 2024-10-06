bab8f847c984865dd741cbc5394d8931f64dcf6e
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-06 18:50:51 +0200

    Update page.tsx

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,17 +1,23 @@
 import { Product } from "@/types/types";
 import { ProductDetails } from "./components/ProductDetails";
 import { ImageSlider } from "./components/ImageSlider";
+import { ItemCarousel } from "./components/ItemCarousel";
+import { Suspense } from "react";
+import { ItemCarouselSkeleton } from "./components/ItemCarouselSkeleton";
 
 export default async function ProductDetailsPage({ params }: { params: { id: string } }) {
-  const res = await fetch(`https://api.escuelajs.co/api/v1/products/${params.id}`);
+  const res = await fetch(`https://dummyjson.com/products/${params.id}`);
   const productDetails: Product = await res.json();
 
   return (
-    <div className="flex min-h-screen w-screen flex-col items-center p-4">
+    <div className="flex min-h-screen w-screen flex-col items-center gap-10 p-4">
       <div className="main-border-color flex w-fit justify-center gap-10 rounded-lg border bg-black p-10">
         <ImageSlider images={productDetails.images} />
         <ProductDetails productDetails={productDetails} />
       </div>
+      <Suspense fallback={<ItemCarouselSkeleton />}>
+        <ItemCarousel />
+      </Suspense>
     </div>
   );
 }
