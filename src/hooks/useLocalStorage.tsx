82ea35f21d1af9a1471c5f0881110a80331f9eb1
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-06 18:50:59 +0200

    Create useLocalStorage.tsx

---
+++ src/hooks/useLocalStorage.tsx
@@ -0,0 +1,15 @@
+export function useLocalStorage() {
+  function setItem(key: string, value: string) {
+    localStorage.setItem(key, value);
+  }
+
+  function getItem(key: string) {
+    return localStorage.getItem(key);
+  }
+
+  function removeItem(key: string) {
+    localStorage.removeItem(key);
+  }
+
+  return { setItem, getItem, removeItem };
+}
