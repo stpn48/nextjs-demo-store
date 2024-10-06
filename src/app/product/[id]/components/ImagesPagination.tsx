4f06e5805a2a075e4b2f845142ac643514d00335
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-06 18:50:44 +0200

    Create ImagesPagination.tsx

---
+++ src/app/product/[id]/components/ImagesPagination.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import React, { Dispatch, SetStateAction } from "react";
+
+type Props = {
+  images: string[];
+  currDisplayingImageIndex: number | null;
+  setCurrDisplayingImageIndex: Dispatch<SetStateAction<number | null>>;
+};
+
+export function ImagesPagination({
+  images,
+  currDisplayingImageIndex,
+  setCurrDisplayingImageIndex,
+}: Props) {
+  if (!images) {
+    return null;
+  }
+
+  return (
+    <div className="flex h-[100px] w-full justify-center gap-4">
+      {images.map((image, index) => (
+        <div key={index} className="h-[100px] w-[100px] overflow-hidden">
+          <Image
+            onClick={() => setCurrDisplayingImageIndex(index)}
+            alt="productImg"
+            width={100}
+            layout="fixed"
+            objectFit="cover"
+            className={`cursor-pointer rounded-md border hover:border-blue-600 ${currDisplayingImageIndex === index ? "border-blue-600" : "border-transparent"}`}
+            height={100}
+            src={image}
+          />
+        </div>
+      ))}
+    </div>
+  );
+}
