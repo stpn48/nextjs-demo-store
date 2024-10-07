d7fb52b9b23803f47712d7527b8d6767887a602e
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-07 14:25:15 +0200

    Create page.tsx

---
+++ src/app/search/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { ProductList } from "./components/ProductList";
+
+export default function SearchPage() {
+  return (
+    <div className="flex flex-col">
+      <ProductList />;
+    </div>
+  );
+}
