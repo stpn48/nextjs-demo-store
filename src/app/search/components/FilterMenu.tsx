89861e549da2b00e5d5430fea1e8a0d8af58a992
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-07 18:17:23 +0200

    Update FilterMenu.tsx

--- src/app/search/components/FilterMenu.tsx
+++ src/app/search/components/FilterMenu.tsx
@@ -32,24 +32,30 @@ export function FilterMenu() {
 
   // update url when filter changes
   useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+
     if (!filter) {
-      router.push("/search");
-      return;
+      params.delete("filter");
+    } else {
+      params.set("filter", filter);
     }
 
-    router.push(`/search?filter=${filter}`);
-  }, [filter]);
+    const newFilter = params.toString() ? `?${params.toString()}` : "/search";
+
+    router.push(newFilter);
+  }, [filter, router]);
 
   // initially set filter from url
   useEffect(() => {
     setFilter(searchParams.get("filter") as FilterType);
-  }, []);
+  }, [searchParams, setFilter]);
 
   return (
     <div className="flex w-fit flex-col gap-2 text-sm text-white">
       <h1 className="text-xl font-bold">Filters</h1>
       {filterChoices.map((choice) => (
         <p
+          key={choice}
           className={`${filter === choice ? "text-white" : "text-secondary"} w-full cursor-pointer whitespace-nowrap hover:text-white`}
           onClick={() => handleChoiceClick(choice)}
         >
