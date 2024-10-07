9a667d19a77fbec3c091df354102245aa557e172
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-07 06:40:28 +0200

    Update ProductDetails.tsx

--- src/app/product/[id]/components/ProductDetails.tsx
+++ src/app/product/[id]/components/ProductDetails.tsx
@@ -10,7 +10,7 @@ export function ProductDetails({ productDetails }: Props) {
     <div className="flex w-[540px] flex-col justify-between gap-4">
       <div className="flex flex-col gap-5">
         <h1 className="text-4xl font-bold text-white">{productDetails.title}</h1>
-        <p className="h-fit w-fit rounded-full bg-blue-600 px-4 py-2 pr-5 text-justify text-white">
+        <p className="h-fit w-fit rounded-full bg-blue-600 px-4 py-1 pr-5 text-justify text-white">
           ${productDetails.price}
         </p>
         <p className="text-secondary">{productDetails.description}</p>
