2facbd5a1b92c4771de1f2c5c5cdb68b735da2b6
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-08 09:10:17 +0200

    Update Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,13 +6,13 @@ import { NewsletterSignup } from "./NewsletterSignup";
 
 export function Footer() {
   return (
-    <div className="font-geistSans flex flex-col text-sm">
-      <div className="main-border-color mt-10 flex h-[300px] items-center gap-20 border-t p-4 px-10 text-sm text-white">
+    <div className="flex w-full flex-col font-geistSans text-sm">
+      <div className="main-border-color mt-10 flex h-[300px] w-full items-center gap-20 border-t p-4 px-10 text-sm text-white">
         <div className="flex items-center gap-2">
           <NavbarBtn className="text-secondary text-sm">Z</NavbarBtn>
-          <h1 className="font-geistMono font-semibold">ZOMRAK STORE</h1>
+          <h1 className="whitespace-nowrap font-geistMono font-semibold">ZOMRAK STORE</h1>
         </div>
-        <div className="flex flex-grow items-center justify-between">
+        <div className="flex w-full items-center justify-between">
           <FooterLinks />
           <NewsletterSignup />
         </div>
@@ -20,7 +20,13 @@ export function Footer() {
       <div className="main-border-color text-secondary flex gap-2 border-t p-5">
         <p className="text-sm">© 2022 Zomrak Store</p>
         <p>|</p>
-        <Link href={"https://github.com/stpn48/nextjs-demo-store"}>View Source</Link>
+        <Link className="hover:text-white" href={"https://github.com/stpn48/nextjs-demo-store"}>
+          View Source
+        </Link>
+        <p>|</p>
+        <Link className="hover:text-white" href={"https://demo.vercel.store"}>
+          Inspired by vercel demo store
+        </Link>
       </div>
     </div>
   );
