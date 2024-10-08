4e045d262f207bc58060999be5f00bd6c296a68f
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-08 18:20:08 +0200

    Update CountryInput.tsx

--- src/app/checkout/information/components/CountryInput.tsx
+++ src/app/checkout/information/components/CountryInput.tsx
@@ -1,9 +1,30 @@
+"use client";
+
 import { Input } from "@/components/Input";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useCheckout } from "../store/useCheckout";
+
 export function CountryInput() {
+  const { country, setCountry, submitInformation } = useCheckout();
+
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    if (submitInformation) {
+      if (!country) {
+        setError(true);
+      }
+    }
+  }, [submitInformation, country]);
+
   return (
     <div className="flex flex-col gap-1">
       <Input
+        value={country}
+        error={error}
+        errorMsg={"Invalid Country"}
+        setError={setError}
+        onChange={(e) => setCountry(e.target.value)}
         required
         requiredMsg="Enter your country"
         wrapperClassName="flex-grow"
