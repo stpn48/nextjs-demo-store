12b0b9e8115e1e9a2397cc601df171ffc42f6e6b
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-08 18:20:05 +0200

    Create ProductOverview.tsx

---
+++ src/app/checkout/components/ProductOverview.tsx
@@ -0,0 +1,29 @@
+import { CartItem } from "@/types/types";
+import Image from "next/image";
+import React from "react";
+
+type Props = {
+  product: CartItem;
+};
+
+export function ProductOverview({ product }: Props) {
+  return (
+    <div className="main-border-color flex border-b py-4">
+      <Image
+        src={product.images[0]}
+        alt={product.title}
+        width={100}
+        height={100}
+        layout="fixed" // Changed from 'fixed' to 'intrinsic'
+        objectFit="cover" // Retains aspect ratio and fills the container
+      />
+      <div className="flex w-full justify-between px-4">
+        <div>
+          <h1 className="text-xl font-bold">{product.title}</h1>
+          <p className="text-secondary">{product.category}</p>
+        </div>
+        <h1 className="flex h-full items-center">${product.price}</h1>
+      </div>
+    </div>
+  );
+}
