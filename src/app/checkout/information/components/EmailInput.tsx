60ce1ab85549d6bb077efbce6de7cf47b9b40b83
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-08 18:20:09 +0200

    Update EmailInput.tsx

--- src/app/checkout/information/components/EmailInput.tsx
+++ src/app/checkout/information/components/EmailInput.tsx
@@ -1,10 +1,34 @@
+"use client";
+
 import { Checkbox } from "@/components/Checkbox";
 import { Input } from "@/components/Input";
+import { useCheckout } from "../store/useCheckout";
+import { useEffect, useState } from "react";
+import { checkIfEmailIsValid } from "@/utils/checkIfEmailIsValid";
 
 export function EmailInput() {
+  const { email, setEmail, submitInformation } = useCheckout();
+
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    if (submitInformation) {
+      const isValid = checkIfEmailIsValid(email);
+
+      if (!isValid) {
+        setError(true);
+      }
+    }
+  }, [submitInformation, email]);
+
   return (
     <>
       <Input
+        error={error}
+        errorMsg={"Invalid Email"}
+        setError={setError}
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
         label="Email"
         required
         requiredMsg="Enter your email"
