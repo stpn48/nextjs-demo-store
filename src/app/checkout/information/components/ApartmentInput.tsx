880f4731b68942b8001477d4bf91046580b1868e
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-09 09:30:45 +0200

    Update ApartmentInput.tsx

--- src/app/checkout/information/components/ApartmentInput.tsx
+++ src/app/checkout/information/components/ApartmentInput.tsx
@@ -5,13 +5,13 @@ import React from "react";
 import { useCheckout } from "../store/useCheckout";
 
 export function ApartmentInput() {
-  const { apartment, setApartment } = useCheckout();
+  const { userDetails, setUserDetails } = useCheckout();
 
   return (
     <Input
-      value={apartment}
+      value={userDetails.apartment}
       errorMsg={"Invalid Apartment"}
-      onChange={(e) => setApartment(e.target.value)}
+      onChange={(e) => setUserDetails({ ...userDetails, apartment: e.target.value })}
       wrapperClassName="w-full"
       className="w-full py-3"
       placeholder="Apartment, building, floor, etc. (optional)"
