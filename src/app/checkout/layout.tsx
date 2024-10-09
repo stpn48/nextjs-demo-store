3bc82615fb4a1055d223ee738876cc86891d56cd
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-09 15:32:52 +0200

    refactor: improve moblie design

--- src/app/checkout/layout.tsx
+++ src/app/checkout/layout.tsx
@@ -8,7 +8,7 @@ export default function CheckoutLayout({ children }: { children: React.ReactNode
     <LocalStorageUserDetailsHandler>
       <div className="flex h-fit w-screen justify-center p-8 font-geistSans text-white">
         <div className="main-border-color flex h-fit w-full max-w-[1400px] flex-col items-center rounded-lg border bg-black xl:flex-row xl:items-stretch">
-          <section className="relative flex min-h-[700px] w-[60%] flex-col gap-4 p-0 xl:p-8">
+          <section className="relative flex min-h-[700px] w-[60%] flex-col gap-4 p-0 pt-10 lg:pt-0 xl:p-8">
             <CheckoutNavigation />
             {children}
           </section>
