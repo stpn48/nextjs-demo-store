da12f29343647fb37fe0faa7f4a03f1139f14dbd
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-09 05:46:06 +0200

    Update layout.tsx

--- src/app/checkout/layout.tsx
+++ src/app/checkout/layout.tsx
@@ -1,11 +1,15 @@
 import React from "react";
-import CartContentSection from "./components/CartContentSection/CartContentSection";
+import CartContentSection from "./components/CartContentSection";
+import { Toaster } from "react-hot-toast";
 
 export default function CheckoutLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-fit w-screen justify-center p-8 font-geistSans text-white">
       <div className="main-border-color flex h-fit w-full max-w-[1400px] rounded-lg border bg-black">
-        {children}
+        <section className="relative flex min-h-[700px] w-[60%] flex-col gap-4 p-8">
+          {children}
+        </section>
+
         <CartContentSection />
       </div>
     </div>
