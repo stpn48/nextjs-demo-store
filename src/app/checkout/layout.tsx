51aed421563a50cf493029ea672b0c213eeaf142
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-10 09:16:33 +0200

    Update layout.tsx

--- src/app/checkout/layout.tsx
+++ src/app/checkout/layout.tsx
@@ -6,8 +6,8 @@ import CheckoutNavigation from "./components/CheckoutNavigation";
 export default function CheckoutLayout({ children }: { children: React.ReactNode }) {
   return (
     <LocalStorageUserDetailsHandler>
-      <div className="flex h-fit w-screen justify-center p-8 font-geistSans text-white">
-        <div className="main-border-color flex h-fit w-full max-w-[1400px] flex-col items-center rounded-lg border bg-black xl:flex-row xl:items-stretch">
+      <div className="flex h-fit w-screen justify-center p-8 font-geistSans text-black dark:text-white">
+        <div className="flex h-fit w-full max-w-[1400px] flex-col items-center rounded-lg border xl:flex-row xl:items-stretch dark:border-[#212121] dark:bg-black">
           <section className="relative flex min-h-[700px] w-[60%] flex-col gap-4 p-0 pt-10 lg:pt-0 xl:p-8">
             <CheckoutNavigation />
             {children}
