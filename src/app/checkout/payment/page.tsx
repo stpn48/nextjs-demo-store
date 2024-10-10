3427bd4948195887fbfebba4966bbae329993a2d
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-10 09:16:35 +0200

    Update page.tsx

--- src/app/checkout/payment/page.tsx
+++ src/app/checkout/payment/page.tsx
@@ -10,9 +10,14 @@ export default function PaymentPage() {
         <DetailsOverview />
         <h1 className="mt-10 text-2xl font-bold">Payment method</h1>
         <p className="text-secondary text-sm">All transactions are securely encrypted.</p>
-        <div className="main-border-color mt-4 flex h-[200px] flex-col items-center justify-center gap-4 rounded-lg border">
-          <Image src={"/errorIcon.svg"} alt="error" width={50} height={50} />
-          <p className="text-secondary w-fit px-4 text-center text-sm">
+        <div className="mt-4 flex h-[200px] flex-col items-center justify-center gap-4 rounded-lg border dark:border-[#212121]">
+          <div className="hidden dark:block">
+            <Image src={"/errorIcon.svg"} alt="error" width={50} height={50} />
+          </div>
+          <div className="block dark:hidden">
+            <Image src={"/errorIconBlack.svg"} alt="error" width={50} height={50} />
+          </div>
+          <p className="w-fit px-4 text-center text-sm text-[#8d8d8d]">
             This store can&apos;t accept payments right now
           </p>
         </div>
