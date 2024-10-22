75923ea3f1f946dbb02239afd23ede5ef7b1780a
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-22 21:32:36 +0200

    Update Navbar.tsx

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,22 +1,27 @@
-import React, { Suspense } from "react";
+import { getUser } from "@/app/actions/getUser";
 import Link from "next/link";
+import React, { Suspense } from "react";
+import { AdminDashboardButton } from "./components/AdminDashboardButton";
+import { CartButton } from "./components/CartButton";
 import { NavbarBtn } from "./components/NavbarBtn";
 import { Searchbar } from "./components/Searchbar";
-import { CartButton } from "./components/CartButton";
 import { ThemeSwitchButton } from "./components/ThemeSwitchButton";
+import { UserButton } from "./components/UserButton";
 
-export function Navbar() {
+export async function Navbar() {
+  const user = await getUser();
+
   return (
     <div
       suppressHydrationWarning
-      className="flex items-center justify-center overflow-x-hidden px-2 py-2 font-geistSans"
+      className="fixed z-10 flex w-screen items-center justify-center bg-white px-2 py-2 font-geistSans dark:bg-[#131313]"
     >
       <div className="text-secondary absolute left-2 top-2 flex items-center gap-4 text-sm">
         <Link prefetch className="hover:text-black dark:hover:text-white" href={"/"}>
           <NavbarBtn>Z</NavbarBtn>
         </Link>
         <Link
-          className="hidden hover:text-black md:block dark:hover:text-white"
+          className="hidden hover:text-black dark:hover:text-white md:block"
           prefetch={true}
           href={"/search"}
         >
@@ -24,7 +29,7 @@ export function Navbar() {
         </Link>
         <Link
           prefetch={true}
-          className="hidden hover:text-black md:block dark:hover:text-white"
+          className="hidden hover:text-black dark:hover:text-white md:block"
           href={"/about"}
         >
           About
@@ -34,7 +39,9 @@ export function Navbar() {
         <Searchbar className="dark:border-[#212121]" searchBarClassName="dark:border-[#212121]" />
       </Suspense>
       <div suppressHydrationWarning className="absolute right-4 top-2 flex items-center gap-2">
+        {user?.role.role === "admin" && <AdminDashboardButton />}
         <ThemeSwitchButton />
+        <UserButton user={user} />
         <CartButton />
       </div>
     </div>
