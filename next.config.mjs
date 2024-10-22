14f444de5cfb5c8a6f1db7946b0f9e3b827ba898
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-22 21:31:55 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ const nextConfig = {
     domains: [
       "images.ctfassets.net", // existing domains
       "cdn.dummyjson.com", // add this line
+      "lh3.googleusercontent.com",
     ],
   },
 };
